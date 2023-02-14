8aa7c486138c9aab9070d99186bd9461951a14eb
Sergey Popov <popow_sv@ukr.net>
2023-02-14 15:59:17 +0200

    my homework change Main

--- src/oopEncapsulation/Main.java
+++ src/oopEncapsulation/Main.java
@@ -5,7 +5,7 @@ public class Main {
         new Employee("Popov Sergey", "developer java", "psv@gmail.com", "+380671232233", 39);
         new Car().start();
         Employee employee=new Employee("Vasya Pupkin", "driver", "jfgkk@ert.com", "+2545647", 15);
-        employee.setPhone("456555");
+        employee.setPhone("45655566");
         System.out.println(employee.getFullName()+employee.getPhone());
     };
 }
