a1781328f98a5929c99b01850ea3438aa8350e1c
Sergey Popov <popow_sv@ukr.net>
2023-02-17 20:22:27 +0200

    change main hw3

--- src/oopEncapsulation/Main.java
+++ src/oopEncapsulation/Main.java
@@ -4,8 +4,6 @@ public class Main {
     public static void main(String[] args) {
         new Employee("Popov Sergey", "developer java", "psv@gmail.com", "+380671232233", 39);
         new Car().start();
-        Employee employee=new Employee("Vasya Pupkin", "driver", "jfgkk@ert.com", "+2545647", 15);
-        employee.setPhone("45655566");
-        System.out.println(employee.getFullName()+employee.getPhone());
+
     };
 }
